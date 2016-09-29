44fbfeaa884465a9e5252b3b509b85d1059fb6f4
SimhachalamJ <jschalam@gmail.com>
2016-09-29 16:21:57 +0530

    changed sw.js

--- sw.js
+++ sw.js
@@ -13,11 +13,41 @@ self.addEventListener('activate', function(event)
 });
 self.addEventListener('push', function(event)
 {
-    console.log('Push message received', event);
-    var title = 'Push message';
+    console.log('Push message received ', event);
+    console.log('Push message data ', event.data);
+    console.log('Push message data.text ', event.data.text());
+    console.log('Push message data.json ', event.data.json());
+    var jsonReponse = event.data.json();
 
-    event.waitUntil(self.registration.showNotification(title, {
-        'body' : 'The Message',
-        'icon' : 'images/logo.png'
+    event.waitUntil(self.registration.showNotification(jsonReponse.title, {
+        'body' : jsonReponse.message,
+        'icon' : jsonReponse.icon,
+        'data' : {
+            'url' : jsonReponse.url,
+        }
+    }));
+});
+self.addEventListener('notificationclick', function(event)
+{
+    console.log('Notification click: tag ', event.notification.tag);
+    console.log('Notification click: ', event.notification);
+    event.notification.close();
+    var url = event.notification.data.url;
+    event.waitUntil(clients.matchAll({
+        type : 'window'
+    }).then(function(windowClients)
+    {
+        for (var i = 0; i < windowClients.length; i++)
+        {
+            var client = windowClients[i];
+            if (client.url === url && 'focus' in client)
+            {
+                return client.focus();
+            }
+        }
+        if (clients.openWindow)
+        {
+            return clients.openWindow(url);
+        }
     }));
 });
